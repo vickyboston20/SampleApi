2b2440c37e588310180ccd7e7622155c7c0c90db
vickyboston20 <vickyboston20@gmail.com>
2019-07-18 12:03:49 +0530

    Added Cors header library

--- web/app.py
+++ web/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 client = MongoClient("mongodb://db:27017")
